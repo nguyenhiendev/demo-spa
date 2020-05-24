e54ec9a27dfe599f0f97879ee2f9ff921482b06d
sadowrock <nguyenhien.nohats@gmail.com>
2020-05-24 18:50:45 +0700

    update

--- components/Header.vue
+++ components/Header.vue
@@ -12,19 +12,19 @@
           <nav class="primary-menu">
             <ul>
               <li>
-                <a href="@/">Home</a>
+                <a href="/">Home</a>
               </li>
               <li>
-                <a href="@/about">About</a>
+                <a href="/about">About</a>
               </li>
               <li>
-                <a href="@/feature">Feature</a>
+                <a href="/feature">Feature</a>
               </li>
               <li>
-                <a href="@/service">Service</a>
+                <a href="/service">Service</a>
               </li>
               <li>
-                <a href="@/contact">Contact</a>
+                <a href="/contact">Contact</a>
               </li>
             </ul>
           </nav>
